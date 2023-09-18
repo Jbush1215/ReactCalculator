b2e53563be414177c1087301a382a83e315b8e72
Jbush1215 <jessierbush1215@gmail.com>
2023-09-18 11:03:41 -0400

    Boiler Plate Code / Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import CalculatorForm from './Components/CalculatorForm/CalculatorForm';
+import NumericDisplay from './Components/NumericDisplay/NumericDisplay';
+import SavedResultList from './Components/SavedResultList/SavedResultlist';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NumericDisplay />
+      <CalculatorForm />
+      <SavedResultList />
     </div>
   );
 }
---
+++ src/Components/SavedResultList/SavedResultlist.jsx
@@ -0,0 +1,5 @@
+import react from 'react';
+
+export default function SavedResultList() {
+    return <div>SavedResultList</div>
+}
